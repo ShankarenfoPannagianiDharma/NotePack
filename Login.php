03c6cd833495da827cc095f019f63e3844b2e669
ShankarenfoPannagianiDharma <dharmas@douglascollege.ca>
2021-09-30 16:38:30 -0700

    Database access & login function
    VERY rudimentary login function in index. Access database with php mysqli, redirects to main or index based on sql query result. Tested using XAMPP localhost.

---
+++ Login.php
@@ -0,0 +1,19 @@
+<?php
+    $accName = $_POST['accountName'];
+    $accPass = $_POST['accountPass'];
+
+    $con = mysqli_connect('localhost', 'root', '','notespack_db');
+
+    $query = "SELECT * FROM users WHERE Handle='".$accName."' AND Password='".$accPass."'";
+    $rs = mysqli_query($con, $query);
+	$flag = false;
+	foreach($rs as $row) {
+		$flag = true;
+	}
+    if($flag){
+        header("Location: Main.html");
+    }
+    else{
+        header("Location: index.html");   
+    }
+?>
