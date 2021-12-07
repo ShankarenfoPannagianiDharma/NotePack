673d765a96f8dd49849e7713de7342d1b885bcb2
ShankarenfoPannagianiDharma <dharmas@douglascollege.ca>
2021-12-06 19:03:27 -0800

    WIP Recommendations

---
+++ RecomendSys.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import neighbors
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+df = pd.read_csv('static/books.csv',error_bad_lines = False)
+
+df.head(10)
+
+df.isnull().sum()
+
+df.describe()
+
+top_ten = df[df['ratings_count'] > 1000000]
+top_ten.sort_values(by='average_rating', ascending=False)
+plt.style.use('seaborn-whitegrid')
+plt.figure(figsize=(9, 7))
+data = top_ten.sort_values(by='average_rating', ascending=False).head(10)
+sns.barplot(x="average_rating", y="title", data=data, palette='mako')
+plt.savefig('static/out1.png')
+
+most_books = df.groupby('authors')['title'].count().reset_index().sort_values('title', ascending=False).head(10).set_index('authors')
+plt.figure(figsize=(9,7))
+ax = sns.barplot(most_books['title'], most_books.index, palette='YlOrBr')
+ax.set_title("Top 10 authors with most books")
+ax.set_xlabel("Total number of books")
+totals = []
+for i in ax.patches:
+    totals.append(i.get_width())
+total = sum(totals)
+for i in ax.patches:
+    ax.text(i.get_width()+.2, i.get_y()+.2,str(round(i.get_width())), fontsize=9,color='black')
+#plt.show()
+plt.savefig('static/out2.png')
+
+most_rated = df.sort_values('ratings_count', ascending = False).head(10).set_index('title')
+plt.figure(figsize=(8,6))
+ax = sns.barplot(most_rated['ratings_count'], most_rated.index, palette = 'inferno')
+totals = []
+for i in ax.patches:
+    totals.append(i.get_width())
+total = sum(totals)
+for i in ax.patches:
+    ax.text(i.get_width()+.2, i.get_y()+.2,str(round(i.get_width())), fontsize=8,color='black')
+#plt.show()
+plt.savefig('static/out3.png')
+
+df.average_rating = df.average_rating.astype(float)
+fig, ax = plt.subplots(figsize=[8,6])
+sns.distplot(df['average_rating'],ax=ax)
+ax.set_title('Average rating distribution for all books',fontsize=20)
+ax.set_xlabel('Average rating',fontsize=13)
+plt.savefig('static/out4.png')
+
+df2 = df.copy()
+
+df2.loc[ (df2['average_rating'] >= 0) & (df2['average_rating'] <= 1), 'rating_between'] = "between 0 and 1"
+df2.loc[ (df2['average_rating'] > 1) & (df2['average_rating'] <= 2), 'rating_between'] = "between 1 and 2"
+df2.loc[ (df2['average_rating'] > 2) & (df2['average_rating'] <= 3), 'rating_between'] = "between 2 and 3"
+df2.loc[ (df2['average_rating'] > 3) & (df2['average_rating'] <= 4), 'rating_between'] = "between 3 and 4"
+df2.loc[ (df2['average_rating'] > 4) & (df2['average_rating'] <= 5), 'rating_between'] = "between 4 and 5"
+
+rating_df = pd.get_dummies(df2['rating_between'])
+language_df = pd.get_dummies(df2['language_code'])
+
+features = pd.concat([rating_df, 
+                    language_df, 
+                    df2['average_rating'], 
+                    df2['ratings_count']], axis=1)
+
+
+from sklearn.preprocessing import MinMaxScaler
+min_max_scaler = MinMaxScaler()
+features = min_max_scaler.fit_transform(features)
+
+model = neighbors.NearestNeighbors(n_neighbors=6, algorithm='ball_tree')
+model.fit(features)
+dist, idlist = model.kneighbors(features)
+
+print("RecommendSystem Generated, ready to go")
+
+def BookRecommender(book_name):
+    book_list_name = []
+    book_id = df2[df2['title'] == book_name].index
+    print(df2.info())
+    print(book_id)
+    book_id = book_id[0]
+    for newid in idlist[book_id]:
+        book_list_name.append(df2.loc[newid].title)
+    return book_list_name
+
+
+
+
+
